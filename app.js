27be97dd906bd3f989bb93f4979e0823fbb59562
vadimstepanov <stepanovvadim472@gmail.com>
2024-01-04 02:55:54 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -66,14 +66,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
 let tg = Telegram.WebApp;
 tg.ready();
-
-
+tg.expand();
 
 document.addEventListener('DOMContentLoaded', function() {
-
-
-    tg.ready(); // Сразу вызываем ready, чтобы сообщить Telegram, что веб-приложение загружено
-    tg.expand();
 
     let backButton = new Telegram.WebApp.BackButton();
 
@@ -81,82 +76,82 @@ document.addEventListener('DOMContentLoaded', function() {
 
         window.history.back();
 
-
-    })
-
-    // Получаем данные пользователя
-    const user = Telegram.WebApp.initDataUnsafe.user;
-    if (user) {
-      // Теперь у нас есть доступ к данным пользователя, например, к имени
-      const username = user.first_name +" "+ user.last_name; // Пример использования имени
-      // Вставляем имя в элемент div с id="username"
-      document.getElementById('username').textContent = username;
-    } else {
-      console.log("Пользовательские данные недоступны.");
-    }
-
+        })
 });
 
-tg.viewportStableHeight();
-tg.isClosingConfirmationEnabled(true);
+    document.addEventListener('DOMContentLoaded', function () {
 
-tg.BackButton.show();
+        // Получаем данные пользователя
+        const user = Telegram.WebApp.initDataUnsafe.user;
+        if (user) {
+            // Теперь у нас есть доступ к данным пользователя, например, к имени
+            const username = user.first_name + " " + user.last_name; // Пример использования имени
+            // Вставляем имя в элемент div с id="username"
+            document.getElementById('username').textContent = username;
+        } else {
+            console.log("Пользовательские данные недоступны.");
+        }
+
+    });
+
+    tg.viewportStableHeight();
+    tg.isClosingConfirmationEnabled(true);
+
+    tg.BackButton.show();
     Telegram.WebApp.onEvent('themeChanged', 'setThemeClass');
     let bg = Telegram.WebApp.colorScheme;
-    if(bg === "light") {
-        
-    }
-    else {
+    if (bg === "light") {
+
+    } else {
 
     }
-
 
 
 // Получаем все кнопки добавления в корзину
-let buttons = document.querySelectorAll(".product-button");
+    let buttons = document.querySelectorAll(".product-button");
 
 // Добавляем обработчик события клика на каждую кнопку
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function(event) {
-        // Отменяем действие по умолчанию (переход по ссылке)
-        event.preventDefault();
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function (event) {
+            // Отменяем действие по умолчанию (переход по ссылке)
+            event.preventDefault();
 
-        // Получаем id товара из атрибута data-id кнопки
-        let id = this.getAttribute("data-id");
+            // Получаем id товара из атрибута data-id кнопки
+            let id = this.getAttribute("data-id");
+
+            // Получаем данные о корзине из локального хранилища или создаем пустой объект, если их нет
+            let cart = JSON.parse(localStorage.getItem("cart")) || {};
+
+            // Увеличиваем количество товара с данным id на 1 или устанавливаем его равным 1, если его нет в корзине
+            cart[id] = (cart[id] || 0) + 1;
+
+            // Сохраняем обновленные данные о корзине в локальное хранилище
+            localStorage.setItem("cart", JSON.stringify(cart));
+
+            // Обновляем счетчик товаров в корзине
+            updateCartCount();
+        });
+    }
+
+// Функция для обновления счетчика товаров в корзине
+    function updateCartCount() {
+        // Получаем элемент счетчика
+        let count = document.querySelector(".cart-count");
 
         // Получаем данные о корзине из локального хранилища или создаем пустой объект, если их нет
         let cart = JSON.parse(localStorage.getItem("cart")) || {};
 
-        // Увеличиваем количество товара с данным id на 1 или устанавливаем его равным 1, если его нет в корзине
-        cart[id] = (cart[id] || 0) + 1;
+        // Считаем общее количество товаров в корзине
+        let total = 0;
+        for (let id in cart) {
+            total += cart[id];
+        }
 
-        // Сохраняем обновленные данные о корзине в локальное хранилище
-        localStorage.setItem("cart", JSON.stringify(cart));
-
-        // Обновляем счетчик товаров в корзине
-        updateCartCount();
-    });
-}
-
-// Функция для обновления счетчика товаров в корзине
-function updateCartCount() {
-    // Получаем элемент счетчика
-    let count = document.querySelector(".cart-count");
-
-    // Получаем данные о корзине из локального хранилища или создаем пустой объект, если их нет
-    let cart = JSON.parse(localStorage.getItem("cart")) || {};
-
-    // Считаем общее количество товаров в корзине
-    let total = 0;
-    for (let id in cart) {
-        total += cart[id];
+        // Устанавливаем текст счетчика равным общему количеству товаров
+        count.textContent = total;
     }
 
-    // Устанавливаем текст счетчика равным общему количеству товаров
-    count.textContent = total;
-}
-
 // Вызываем функцию обновления счетчика при загрузке страницы
-updateCartCount();
+    updateCartCount();
 
 
